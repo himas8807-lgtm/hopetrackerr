c2076b177bca5d4d50035b25efbbcbcd071fa220
himas8807-lgtm <himas8807@gmail.com>
2026-03-04 14:06:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5692,7 +5692,7 @@ async function renderPageByRoute(pageName) {
     loading(true);
     try {
         switch(pageName) {
-            case 'home': await renderhome(); break;
+            case 'home': await renderHome(); break;
             case 'profile': await renderProfile(); break;
             case 'rankings': await renderRankings(); break;
             case 'goals': await renderGoals(); break;
